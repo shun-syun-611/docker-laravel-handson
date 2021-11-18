fd96103f6f10e936cda7233ea69eedc296ca330c
Shun Sugawara <82685781shun-syun-611@users.noreply.github.com>
2021-11-18 22:02:41 +0900

    feat create drop menu list

--- backend/resources/views/layouts/app.blade.php
+++ backend/resources/views/layouts/app.blade.php
@@ -58,14 +58,10 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('home') }}">
-                                    {{ __('home') }}
-                                    </a>
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+                                    <a class="dropdown-item" href="{{ route('home') }}">home</a>
+                                    <a class="dropdown-item" href="{{ route('output') }}">output</a>
+                                    <a class="dropdown-item" href="{{ route('input') }}">input</a>
+                                    <a class="dropdown-item" href="{{ route('logout') }}"onclick="event.preventDefault();document.getElementById('logout-form').submit();">Logout</a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
