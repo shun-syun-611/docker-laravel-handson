c98f58cd0af4f5aa8b518ab9527de0d3597390c6
Shun Sugawara <82685781shun-syun-611@users.noreply.github.com>
2021-11-21 17:33:50 +0900

    feat create url-creating

--- backend/resources/views/contents/detail.blade.php
+++ backend/resources/views/contents/detail.blade.php
@@ -8,13 +8,13 @@
 @endif
 
 <p>投稿ID: {{$item['id']}}</p>
-<p>投稿内容: {{$item['content']}}</p>
+<p>投稿内容: {!! nl2br(preg_replace('/(https?:\/\/[^\s]*)/', '<a href="$1" target="_blank" rel="noopener noreferrer">$1</a>', $item['content'])) !!}</p>
 <p>投稿時間: {{$item['created_at']}}</p>
 <a href="{{route('edit', ['content_id' => $item['id']])}}">編集</a>
 <form action="{{route('delete')}}" method="post">
     @csrf
     <input type="hidden" name="id" value="{{$item['id']}}">
-    <input type="submit" value="削除" onclick='return confirm("削除しますか？");'>
+    <input type="submit" value="削除" onclick='return confirm("本当に削除しますか？");'>
 </form>
 </div>
 @endsection
--- backend/resources/views/contents/output.blade.php
+++ backend/resources/views/contents/output.blade.php
@@ -7,8 +7,9 @@
     @if (isset($item['file_path']))
 <img src="{{asset('storage/' . $item['file_path'])}}" alt="{{asset('storage/' . $item['file_path'])}}">
 @endif
-    <p>{{$item['content']}}</p>
+    <p>{!! nl2br(preg_replace('/(https?:\/\/[^\s]*)/', '<a href="$1" target="_blank" rel="noopener noreferrer">$1</a>', $item['content'])) !!}</p>
     <a class="text-right d-block"  href="{{route('detail', ['content_id' => $item['id']])}}">詳細</a>
+    <a  class="text-right d-block"href="{{route('edit', ['content_id' => $item['id']])}}">編集</a>
 @endforeach
 </div>
 @endsection
